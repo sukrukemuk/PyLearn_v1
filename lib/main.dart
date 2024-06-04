2a4a805a27bc1cd4eedf02282d8808098ffbbb37
Şükrü Kemük <sukrukemuk@gmail.com>
2024-06-04 12:14:52 +0300

    04.06.2024

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PyLearn-v1',
+      title: 'PyLearn',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
